3f7cf41a214b0aa0818cb213676c8c212dec4587
nafiswatsiq <amrullohnafis@gmail.com>
2024-02-24 18:55:04 +0700

    first commit

---
+++ app/Views/home.php
@@ -0,0 +1,42 @@
+<?= $this->extend('layouts/app') ?>
+
+<?= $this->section('content') ?>
+<div class="container mt-5">
+  <div class="row g-2 g-lg-3">
+
+    <div class="col-4">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<?= $this->endSection() ?>
---
+++ app/Views/layouts/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar bg-primary" data-bs-theme="dark">
+  <div class="container">
+    <a class="navbar-brand">Navbar</a>
+    <!-- <form class="d-flex" role="search"> -->
+      <!-- <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"> -->
+      <button class="btn btn-warning" type="submit">Tambah</button>
+    <!-- </form> -->
+  </div>
+</nav>
